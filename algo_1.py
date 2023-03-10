bd36ee1782b340ff565e18c0b7280b55f96cdb6f
anii76 <ka_bourouina@esi.dz>
2023-03-10 02:23:28 +0100

    first ready to run version

--- algo_1.py
+++ algo_1.py
@@ -1,5 +1,6 @@
 import numpy as np
-import scipy as sc
+from scipy.linalg import svd
+import cvxpy as cp
 
 from testing_1 import *
 
@@ -17,6 +18,12 @@ H_AI = H_AI[0].T # NxM
 
 # epsilon 
 epsilon = 1e-3
+
+def secrecy_rate(w,q):
+    Q = np.diag(q)
+    ue_rate = np.log2(1 + (abs((h_IU @ Q @ H_AI + h_AU) @ w)/noise_power)**2)
+    eve_rate = np.log2(1 + (abs((h_IE @ Q @ H_AI + h_AE) @ w)/noise_power)**2)
+    return ue_rate - eve_rate #if ue_rate > eve_rate else 0 # array shape (1,)
 
 # matrices A and B (9),(8)
 def A(q):
@@ -44,8 +51,7 @@ def U_max(B, A):
     
     return np.array([normalized_eig]).T # to have consistent shape of (4,1)
 
-
-
+# solve problem (22) using SDR
 def problem_22(w,q):
     h_U = (h_AU.conjugate() @ w.conjugate() @ w.T @ h_AU.T) / noise_power**2
     h_E = (h_AE.conjugate() @ w.conjugate() @ w.T @ h_AE.T) / noise_power**2
@@ -67,9 +73,12 @@ def problem_22(w,q):
 
     G_E = np.concatenate((np.concatenate((bloc_1, bloc_2), axis=1), bloc_4), axis=0) # equation (17)
 
-    E = [np.zeros((i,i)) for i in range(1,N+2)] # (a 3 dimentional matrix (i,j,n)) n : {1,26}
-    for i in range(N):
-        E[i][i][i] = 1 
+
+     #E = [np.zeros((i,i)) for i in range(1,N+2)] # (a 3 dimentional matrix (i,j,n)) n : {1,26}
+    E_n = np.zeros((N+1,N+1)) # 26
+    E = np.stack([E_n] * N+1) 
+    for i in range(N+1):
+        E[i][i][i] = 1
     #  s[:-1].T.conjugate() @ E[24] @ s[:-1] verifies == 1 (n=25)
 
     ######################
@@ -80,33 +89,76 @@ def problem_22(w,q):
     s = np.append(q,1) # q ki ydkhol rah déjà q.T
     s = np.array([s]).T
     S = s @ s.T.conjugate() # S ~= ss^H (deta equal) # so a scalar not a matrix ? # after adjustments its a matrix of dim(s)xdim(s)
-    # np.linalg.matrix_rank(S) verifies 1
+                            # np.linalg.matrix_rank(S) verifies 1
 
     µ = 1/(np.trace(G_E @ S) + h_E + 1) # µ = µ[0][0] 
     X = µ * S # la variable à optimiser;
 
-    #return S, G_U, G_E, µ, X, h_U, h_E, E
-    objective_function = np.trace(G_U @ X) + µ * (h_U + 1)
-    constraint_1 = np.trace(G_E @ X) + µ * (h_E + 1) # == 1
-    constraint_2 = np.trace(E[n] @ X) # == µ, quelque soit n
+    # Code for reference
+    #objective_function = np.trace(G_U @ X) + µ * (h_U + 1)
+    #constraint_1 = np.trace(G_E @ X) + µ * (h_E + 1) # == 1
+    #constraint_2 = np.trace(E[n] @ X) # == µ, quelque soit n
 
-    # rank(S) = 1, but resulted S is not of rank 1 
+    ## Defining optimisation variable X and µ (X.shape) <=> (N+1,N+1)
+    Z_r = cp.Variable((N+1, N+1), PSD=True)
+    Z_i = cp.Variable((N+1, N+1), PSD=True)
 
-# So we apply: The standard Gaussian randomization method 
+    µ_r = cp.Variable(nonneg=True)
+    µ_i = cp.Variable(nonneg=True)
+
+    # Not sure the utility of this 
+    Z = cp.hstack([cp.vstack([Z_r, -Z_i]), cp.vstack([Z_i, Z_r])])
+
+    ## Defining opjective function
+    f = ( cp.trace(G_U.real @ Z_r - G_U.imag @ Z_i) + cp.imag(cp.trace(G_U.real @ Z_i + G_U.imag @ Z_r))+ µ_r * cp.real(h_U + 1) - µ_i * cp.imag(h_U + 1))
+
+    ## Defining constraints
+    # 1) tr(G_E @ x) + µ * (h_E + 1) == 1
+    constraints = [ cp.trace(G_E.real @ Z_r - G_E.imag @ Z_i) + 1j * cp.trace(G_E.real @ Z_i + G_E.imag @ Z_r) + µ_r * cp.real(h_E + 1) - µ_i * cp.imag(h_E + 1) == 1 ]
+
+    # 2) tr(E[n] @ X) == µ , ∀n
+    for n in range(26):
+        constraints.append(cp.trace(E[n] @ Z_r) == µ_r)
+        constraints.append(cp.trace(E[n] @ Z_i) == µ_i)
+    
+    ## Defining optimization problem
+    problem = cp.Problem(cp.Maximize(f), constraints)
+
+    ## Solve problem
+    res = problem.slove(solver='SCS')
+
+    # Optimized values of X and µ
+    X_ = (Z_r + 1j * Z_i).value
+    µ_ = (µ_r + 1j * µ_i).value
+
+    # Value of S
+    S_ = X_/µ_
+
+    # Normally rank(S) = 1, but resulted S is not of rank 1 
+    rankS = np.linalg.matrix_rank(S_)
+
+    return S_
+    
+# So we apply: The standard Gaussian randomization method (kyn bzzf; 9rinahum f ANUM (1CS))
 # to obtain an approximate solution to the matrix decomposition problem
 # S = ss^H , finding s helps us extract q.
 def sgr(S):
-    # calculate approx_s and approx_q
+    # Calculate s_approx and q_approx
+
+    m = 5 # Number of random projections
+    Y = np.random.rand(N+1,m)
+    Z = S @ Y
+    Q, R = np.linalg.qr(Z, mode='reduced')
+    U, Sigma, VT = svd(Q.T @ S @ Q)
+
+    # Extract the dominant singular vector
+    s_approx = np.sqrt(Sigma[0]) * Q @ VT[0]
+
     # I'm quite confused how to get the value of q ?
-    test = S
+    # ChatGPT told me that q = s[:-1] :))
+    q_approx = s_approx[:-1]
 
-
-def secrecy_rate(w,q):
-    Q = np.diag(q)
-    ue_rate = np.log2(1 + (abs((h_IU @ Q @ H_AI + h_AU) @ w)/noise_power)**2)
-    eve_rate = np.log2(1 + (abs((h_IE @ Q @ H_AI + h_AE) @ w)/noise_power)**2)
-    return ue_rate - eve_rate #if ue_rate > eve_rate else 0 # array shape (1,)
-
+    return q_approx
 
 
 if __name__ == '__main__' :
@@ -118,10 +170,10 @@ if __name__ == '__main__' :
     q_0 = np.ones(N) # Nx1
     r_0 = secrecy_rate(w_0,q_0) #objective value
 
-    # to keep track of all changes
-    w = []; w.append(w_0)
-    q = []; q.append(q_0)
-    r = []; r.append(r_0)
+    # to keep track of all changes(hoping we don't surpass 100 iterations)
+    w = []*100; w.append(w_0)
+    q = []*100; q.append(q_0)
+    r = []*100; r.append(r_0)
 
     # Transform this into repeat-until 
     while (r[k] - r[k-1])/r[k] >= epsilon :
@@ -130,6 +182,9 @@ if __name__ == '__main__' :
         µ_max = U_max(A(q[k-1]),B(q[k-1])) 
         w[k] = np.sqrt(transmit_power)*µ_max
         # With w[k] : solve (problem (22)) => get(S) which it's rank(S)# 1
+        S = problem_22(w[k],q[k-1])
         #             apply gussian randomisation over solution to obtain approx. q[k]
+        q[k] = sgr(S)
         r[k] = secrecy_rate(w[k],q[k])
+        print(k+". Secrecy : "+r[k])
 
