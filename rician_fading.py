71290d3ae78918639478f7b360e21c4ec4e84446
anii76 <ka_bourouina@esi.dz>
2023-04-01 13:37:59 +0100

    correct fading and spatial correlation

--- rician_fading.py
+++ rician_fading.py
@@ -1,17 +1,52 @@
 import numpy as np
+from scipy.linalg import sqrtm
+# all channal coefficients are in dB ?
 
 #M is the number of transmitter's antennas
 #N is the number of receiver' antennas
 #dist is the distance between the transmitter and the receiver
 #pl is path-loss exponent
+#zeta is the pathloss at the reference distance
 #K is the Rician factor
-#eta is the pathloss at the reference distance
+#r is correlation coefficient
+#https://github.com/havutran/Rician-fading-by-Python
+#https://dsp.stackexchange.com/questions/55719/channel-model-los-component-and-rician-k-factor
 
-def Rician_Fading_Channels(M,N,dist,pl,eta,K):
-    mu = np.sqrt(K/((K+1))) #direct path
-    s = np.sqrt(1/(2*(K+1))) #scattered path
-    Hw = mu + s*(np.random.randn(M,N)+1j*np.random.randn(M,N)) # Rician channel
-    H = np.sqrt(eta/(dist**pl))*Hw #Rician channel with pathloss
+
+def Rician_Fading_Channels(M,N,dist,pl,K):
+    path_loss = 1/(dist**pl)
+    #d0 = 1; path_loss = zeta + 10 * pl * np.log10(dist/d0) #should i keep this or go back to path_loss = 1/(dist**pl)
+
+    mu = np.sqrt(K/((K+1))) #direct path (mean)
+    s = np.sqrt(1/(2*(K+1))) #scattered path (sigma) [Standard deviation]
+
+    phi = np.random.uniform(0, 2*np.pi) # instead of AoA and doppler shift (or consider them all equal to 0 )
+    Hw_LOS = mu * np.exp(1j * phi) #* np.exp(1j * f_D * cos(AoA) + phase_shift) (https://www.gaussianwaves.com/2020/08/rician-flat-fading-channel-simulation/)
+    Hw_NLOS = s * (np.random.randn(N,M) + 1j * np.random.randn(N,M)) 
+    Hw = Hw_LOS + Hw_NLOS # Rician channel
+    H = np.sqrt(path_loss)*Hw #Rician channel with pathloss
     return H
 
-# def Spacially_Correlated_Rician():
+
+def Spatially_Correlated_Rician(M,dist,pl,zeta,K,r):
+    # Path loss
+    d0 = 1 
+    zeta = 10**(zeta/10)
+    path_loss = zeta*(d0/dist)**pl
+
+    # Small-scale fading component
+    phi = np.random.uniform(0, 2*np.pi)
+    g_LOS = np.sqrt(K/(K+1)) * np.exp(1j * phi) #because AoA is 0 (00,0),(145,0),(150,0) ?
+    g_NLOS = np.sqrt(1/(2*(K+1))) * (np.random.randn(M) + 1j * np.random.randn(M))
+    R = np.zeros((M,M))
+    for i in range(M):
+        for j in range(M):
+            R[i,j] = r ** np.abs(i-j) #not sure of this line (applying correlation)
+                                            #probably correct (see how to add variance to random distribution in numpy: https://numpy.org/doc/stable/reference/random/generated/numpy.random.randn.html#numpy.random.randn)
+    B = sqrtm(R)
+    small_scale_fading =  g_LOS + B @ g_NLOS
+
+    # Channel coefficients
+    H = np.sqrt(path_loss) * small_scale_fading  
+                #converting to linear units
+    return np.array([H])
--- testing_1.py
+++ testing_1.py
@@ -1,24 +1,47 @@
 import numpy as np
 
-from rician_fading import Rician_Fading_Channels
+from rician_fading import *
 
-K = 1 # Rician factor
-eta = 1 #-30 dB , for the sake of problems (negative number under sqrt) keep it 1
-M = 4 # antenna at BS
-n = 1 # antenna at UE/EVE
-N = 25 # RIS elements
-dist_AU = np.sqrt((150 - 0)**2 + (0 - 0)**2)
-dist_AE = np.sqrt((145 - 0)**2 + (0 - 0)**2)
-dist_AI = np.sqrt((145 - 0)**2 + (5 - 0)**2)
-dist_IE = np.sqrt((145 - 145)**2 + (0 - 5)**2)
-dist_IU = np.sqrt((150 - 145)**2 + (0 - 5)**2)
+K = 1  # Rician factor
+zeta = -30 # -30 dB 
+M = 4  # antenna at BS
+n = 1  # antenna at UE/EVE
+N = 25  # RIS elements
+dist_AU = np.sqrt((150 - 0)**2 + (0 - 0)**2)   # 150m 
+dist_AE = np.sqrt((145 - 0)**2 + (0 - 0)**2)   # 145m
+dist_AI = np.sqrt((145 - 0)**2 + (5 - 0)**2)   # 145.08...m
+dist_IE = np.sqrt((145 - 145)**2 + (0 - 5)**2) # 5m
+dist_IU = np.sqrt((150 - 145)**2 + (0 - 5)**2) # 10m
 pl = 3
 pl_AI = 2.2
+r_factor = 0.95 # correlation factor
 
-N_samples = 1000 # Channel realisations
+# 1000 Channel realisations
+N_samples = int(input('Enter number of samples >>> '))
 
-H_AU = np.array([Rician_Fading_Channels(M,n,dist_AU,pl,eta,K) for _ in range(N_samples)])
-H_AE = np.array([Rician_Fading_Channels(M,n,dist_AE,pl,eta,K) for _ in range(N_samples)])
-H_AI = np.array([Rician_Fading_Channels(M,N,dist_AI,pl_AI,eta,K) for _ in range(N_samples)])
-H_IU = np.array([Rician_Fading_Channels(N,n,dist_IU,pl,eta,K) for _ in range(N_samples)])
-H_IE = np.array([Rician_Fading_Channels(N,n,dist_IE,pl,eta,K) for _ in range(N_samples)])
+
+H_AU = np.array([Spatially_Correlated_Rician(M, dist_AU, pl, zeta, K, r_factor)
+                for _ in range(N_samples)])
+H_AE = np.array([Spatially_Correlated_Rician(M, dist_AE, pl, zeta, K ,r_factor)
+                for _ in range(N_samples)])
+
+#H_AU = np.array([Rician_Fading_Channels(M, 1, dist_AU, pl, K)
+#                for _ in range(N_samples)])
+#H_AE = np.array([Rician_Fading_Channels(M, 1, dist_AE, pl, K)
+#                for _ in range(N_samples)])
+H_AI = np.array([Rician_Fading_Channels(M, N, dist_AI, pl_AI, K)
+                for _ in range(N_samples)])
+H_IU = np.array([Rician_Fading_Channels(N, n, dist_IU, pl, K)
+                for _ in range(N_samples)])
+H_IE = np.array([Rician_Fading_Channels(N, n, dist_IE, pl, K)
+                for _ in range(N_samples)])
+
+Channels = {
+    "H_AU": H_AU, 
+    "H_AE": H_AE,
+    "H_AI": H_AI,
+    "H_IU": H_IU,
+    "H_IE": H_IE
+    }
+
+np.save('Channels',Channels)
